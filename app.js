22cf35fefe056af1f164c7034e7576a968c74d2c
Erick Watanabe <erickjwatanabe@gmail.com>
2022-03-01 16:16:07 -0800

    prompt.js file set up, display of prompt set, listen function for the enter button set.

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('app.js working');
+$(document).ready(function () {
+    /* -- Select already existing elements -- */
+    var $gameboard = $('#gameboard');
+    $gameboard.html('');
+
+    // console.log(problems); -> works
+    // js vars to use.
+    var vi = problems[0].vi;
+    var xf = problems[0].xf;
+    /* -- Create new HTML elements -- */
+    var $vi = $(`<p>Inital Velocity: ${vi} m/s</p>`);
+    var $xf = $(`<p>Target Distance: ${xf} m</p>`);
+    var $ans = $('<textarea id="ans" placeholder="Answer for theta"></textarea>');
+    // setting a title for the answer box - did not work
+    var $ansLabel = $('<label for="ans">Type answer: </label>');
+    var $enterANS = $('<button>Enter</button>');
+    
+    /* -- Create event handler functions -- */
+    // To check if the answer is correct
+    function enterANS(enterEvent) {
+        alert('enter button has been clicked');
+        // how to grab data from text box & compare the answer to the correct one
+    }
+    /* -- Set event listeners (providing appropriate handlers as input) -- */
+    // listen for the submitting on the Enter button
+    $enterANS.on('click', enterANS);
+    /* -- Append new HTML elements to the DOM -- */
+    $vi.appendTo($gameboard);
+    $xf.appendTo($gameboard);
+    $ansLabel.appendTo($gameboard);
+    $ans.appendTo($gameboard);
+    $enterANS.appendTo($gameboard);
+
+});
---
+++ prompt.js
@@ -0,0 +1,15 @@
+// The prompts and answers to check against will go here 
+// Similar to tweets generation, but without the random.
+// Will make an object for each problem, and put them in an array
+// will generate a random number to select problem to be displayed in app.js
+
+window.problems = {};
+var problems = [];
+
+var problem1 = {
+    vi: 15,
+    xf: 20,
+    theta: 60
+}
+window.problems.push(problem1);
+
