8c1d918f37d8370b06d05bdffad9a2b3f2c6102f
Erick Watanabe <erickjwatanabe@gmail.com>
2023-01-04 12:19:09 -0800

    set up index.jsx

---
+++ client/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class App extends React.Componenet {
+  constructor (props) {
+    super(props);
+
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
