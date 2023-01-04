2bb0907f7d2edeb880ec46bc8b1b3a1845c3dd1d
Erick Watanabe <erickjwatanabe@gmail.com>
2023-01-04 13:26:34 -0800

    server localhost running, but bundle (network says buddle.js) is not loading, getting 404

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 
 class App extends React.Componenet {
   constructor (props) {
     super(props);
 
+  }
+
+  // make a new Game obj for each round
+
+
+  render () {
+    return (
+      <div>
+        <h1>Learning Physics and Math</h1>
+        <GenBoardSize />
+        <UserInput />
+      </div>
+    )
   }
 }
 
---
+++ server/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.listen(3000, function() {
+  console.log('Server started and listening on port 3000');
+});
